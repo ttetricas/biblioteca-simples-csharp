d22de578a1d8be463e7a02c6296b2f1642a7e254
ttetricas <estermeireles89@gmail.com>
2025-06-17 00:55:52 -0300

    add sistema de cadastro de livros

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string resposta = "s";
+
+while (resposta == "s")
+{
+    string livro;
+
+    do
+    {
+        Console.Write("Digite o nome do livro: ");
+        livro = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(livro))
+        {
+            Console.WriteLine("Por favor, digite um nome válido.");
+        }
+    } while (string.IsNullOrWhiteSpace(livro));
+
+    Console.WriteLine("Livro cadastrado: " + livro);
+
+    do
+    {
+        Console.Write("Quer cadastrar outro livro? (s/n): ");
+        resposta = Console.ReadLine().ToLower();
+        if (resposta != "s" && resposta != "n")
+        {
+            Console.WriteLine("Por favor, responda só com 's' ou 'n'.");
+        }
+    } while (resposta != "s" && resposta != "n");
+}
